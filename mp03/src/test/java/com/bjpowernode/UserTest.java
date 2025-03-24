7bc27410137f075746550a9b898c20ab8faf1723
MathewTang <mathewtang@qq.com>
2025-03-24 17:54:03 +0800

    2025.03.23 21:02 MybatisPlus第六次提交 5.6MybatisX插件使用，注意*.xml目录生成，使用实体类别名，组件命名及注入，<sql>属性、<ResultMap>、

---
+++ mp03/src/test/java/com/bjpowernode/UserTest.java
@@ -0,0 +1,41 @@
+package com.bjpowernode;
+
+import com.bjpowernode.domain.User;
+import com.bjpowernode.mapper.UserMapper;
+import com.bjpowernode.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+// @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) //启动类 SpringBoot
+@SpringBootTest
+class UserTest {
+    // @Qualifier("userMapper1")
+    // @Autowired
+    @Resource(name = "userMapper1")
+    private UserMapper userMapper;
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 记录一下，我用MybatisX生成项目结构时，*.xml那里，我写了com.bjpowernode.mapper
+     *  但是只生成了mapper，于是我使用Shift+F6写了com.bjpowernode.mapper
+     *  科室我忘记了他是目录，不是包，用那个只会生成一个目录
+     *  要使用Directory，输入com\bjpowernode\mapper
+     */
+    @Test
+    void testSelectByNameUser() {
+        User tom = userMapper.selectByNameUser("Tom");
+        System.out.println(tom);
+    }
+
+    @Test
+    void testMapper() {
+        List<User> users = userMapper.selectList(null);
+        System.out.println(users);
+    }
+}
